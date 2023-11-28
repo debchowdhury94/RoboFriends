72538419eb1bfd66e12e12c2bd3b240c7fd41059
debchowdhury94 <debchowdhury94@gmail.com>
2023-11-29 01:29:28 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Fragment>
+      {
+        robots.map((robot)=> {
+          return (
+            <Card id={robot.id} name={robot.name} email={robot.email} />
+          )
+        })
+      }
+    </React.Fragment>
   </React.StrictMode>
 );
 
